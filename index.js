46ba44b546401e2113b7f8e6d9fe9c38623f2445
Vijay Krishan <vk856549@gmail.com>
2024-08-29 23:52:50 +0530

    added studyPlanRoute

--- index.js
+++ index.js
@@ -1,41 +1,45 @@
-require('dotenv').config();
+require("dotenv").config();
 
-const cors = require('cors')
-const express = require('express')
-const { connectDB } = require('./config/database.js')
-const cookieParser = require('cookie-parser')
+const cors = require("cors");
+const express = require("express");
+const { connectDB } = require("./config/database.js");
+const cookieParser = require("cookie-parser");
 
-const userRoute = require('./routes/userRoute.js')
-const courseRoute = require('./routes/courseRoute.js')
-const reviewRoute = require('./routes/reviewRoute.js')
-const cartRoute = require('./routes/cartRoute.js')
-const categoryRoute = require('./routes/categoryRoute.js')
-const AssignmentRoute = require('./routes/assignmentRoute.js')
+const userRoute = require("./routes/userRoute.js");
+const courseRoute = require("./routes/courseRoute.js");
+const reviewRoute = require("./routes/reviewRoute.js");
+const cartRoute = require("./routes/cartRoute.js");
+const categoryRoute = require("./routes/categoryRoute.js");
+const AssignmentRoute = require("./routes/assignmentRoute.js");
+const StudyPlanRoute = require("./routes/studyPlanRoute.js");
 
-const authRoute = require('./routes/authRoute.js')
+const authRoute = require("./routes/authRoute.js");
 
-const app = express()
-const port = process.env.PORT
+const app = express();
+const port = process.env.PORT;
 
 connectDB();
 
-app.use(cors({
+app.use(
+  cors({
     credentials: true,
-    origin: true
-  }))
-  
-  app.use(express.json())
-  app.use(cookieParser())
+    origin: true,
+  })
+);
 
-app.use('/user', userRoute)
-app.use('/course', courseRoute)
-app.use('/rating', reviewRoute)
-app.use('/cart', cartRoute)
-app.use('/category', categoryRoute)
-app.use('/assignment',AssignmentRoute)
+app.use(express.json());
+app.use(cookieParser());
 
-app.use('/auth' , authRoute)
+app.use("/user", userRoute);
+app.use("/course", courseRoute);
+app.use("/rating", reviewRoute);
+app.use("/cart", cartRoute);
+app.use("/category", categoryRoute);
+app.use("/assignment", AssignmentRoute);
+app.use("/studyPlan", StudyPlanRoute);
 
-app.listen(port , ()=> {
-    console.log(`App Listening on port ${port}`)
-})
+app.use("/auth", authRoute);
+
+app.listen(port, () => {
+  console.log(`App Listening on port ${port}`);
+});
